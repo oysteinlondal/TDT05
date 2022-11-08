41cb24adc4915572653792f2d251f4e0fcccb2ff
oysteinlondal <oystein@nenett.no>
2022-11-08 17:11:39 +0100

    Implemented the general pipeline

---
+++ train.py
@@ -0,0 +1,34 @@
+from flair.datasets import CONLL_03
+from flair.embeddings import TransformerWordEmbeddings
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+
+corpus = CONLL_03()
+print(corpus)
+
+label_type = 'ner'
+
+label_dict = corpus.make_label_dictionary(label_type=label_type)
+print(label_dict)
+
+embeddings = TransformerWordEmbeddings(model='xlm-roberta-large',
+                                       layers="-1",
+                                       subtoken_pooling="first",
+                                       fine_tune=True,
+                                       use_context=True,
+                                       )
+
+tagger = SequenceTagger(hidden_size=256,
+                        embeddings=embeddings,
+                        tag_dictionary=label_dict,
+                        tag_type='ner',
+                        use_crf=False,
+                        use_rnn=False,
+                        reproject_embeddings=False,
+                        )
+trainer = ModelTrainer(tagger, corpus)
+
+trainer.fine_tune('resources/taggers/sota-ner-flert',
+                  learning_rate=5.0e-6,
+                  mini_batch_size=4,
+                )
